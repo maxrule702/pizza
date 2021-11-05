70b0a9f20c943d10cfc637a98650a613e0330669
maxrule702 <maximoo2004@gmail.com>
2021-11-05 11:00:12 +0000

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,29 +13,29 @@ public class Main {
         Scanner input = new Scanner(System.in);
         while (placeholder1 == 0) {
             System.out.println("what size pizza would you like?");
-            String pizzaSize = input.next();
-            if (pizzaSize.equals("small")) {
-                pizzaPrice = pizzaPrice + 7.99;
-            }
-            if (pizzaSize.equals("medium")) {
-                pizzaPrice = pizzaPrice + 10;
-            }
-            if (pizzaSize.equals("large")) {
-                pizzaPrice = pizzaPrice + 13.99;
+            while (true) {
+                String pizzaSize = input.next();
+                pizzaSize = pizzaSize.toLowerCase();
+                if (pizzaSize.equals("small")) {
+                    pizzaPrice = pizzaPrice + 7.99;
+                    break;
+                }
+                if (pizzaSize.equals("medium")) {
+                    pizzaPrice = pizzaPrice + 10;
+                    break;
+                }
+                if (pizzaSize.equals("large")) {
+                    pizzaPrice = pizzaPrice + 13.99;
+                    break;
+                }
+                else{
+                    System.out.println("invalid respones try again");
+                }
             }
 
             while (placeholder != 1) {
-
-                    System.out.println("please enter your toppings and type done when finshed. extra topping cost 1.25");
-
-                    try {
-                        topping = input.next();
-                    } catch (InputMismatchException e) {
-                        System.out.println("numbers not allowed");
-                        topping = input.next();
-                    }
-
-
+                System.out.println("please enter your toppings and type done when finshed. extra topping cost 1.25");
+                topping = input.next();
                 if (count > 2) {
                     pizzaPrice = pizzaPrice + 1.25;
                 }
